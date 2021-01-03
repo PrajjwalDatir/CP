15c40e57acaaaf6b5a21cddec9b4ca325695f6be
PrajjwalDatir <prajjwaldatir0072000@gmail.com>
2021-01-03 18:40:48 +0530

    Q 1/450 added

--- 450/001.cpp
+++ 450/001.cpp
@@ -1,7 +1,42 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-  int arr[1];
+void printArray(int arr[], int SIZE){
+  for (int i=0; i<SIZE; i++){
+    cout << arr[i] << ",";
+  }
+  cout << endl;
+  return;
+}
+
+void reverseArray(int arr[], int start, int end){
+  while (start < end)
+  {
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start ++;
+    end --;
+  }
+
+  return;
+}
+
+
+int main()
+{
+  int process[] = {1, 2, 3, 4, 5, 6, 7, 8 ,9 , 10};
+  int SIZE = sizeof(process)/sizeof(process[0]);
+  printArray(process, SIZE);
+
+  reverseArray(process, 0, SIZE - 1);
+
+  printArray(process, SIZE);
+
+// code
+
+
+
+  return 0;
 
 }
