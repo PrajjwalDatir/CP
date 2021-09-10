2124049d13d6ba223d9351972e11d3281ffaf1e4
PrajjwalDatir <prajjwaldatir0072000@gmail.com>
2021-09-10 15:06:59 +0530

    lot of types! & print like C

--- GoPractice/reVar.go
+++ GoPractice/reVar.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
 	constVar := "I do not change"
 	const stable int = 10
-
-	fmt.Println(constVar, stable)
+	var smallInt int32
+	var bigInt int64 = 1361416
+	fmt.Printf("constVar %s & stable %s\n", constVar, "new")
+	fmt.Printf("smallInt %d & bigInt %d\n", smallInt, bigInt)
 }
